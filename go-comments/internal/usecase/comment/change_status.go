6ef7efa0d027d01c34f408ba8897d9bb458df41e
ee-crocush <eugene.mogush@gmail.com>
2025-08-10 01:03:54 +0500

    Подправили нейминг

--- go-comments/internal/usecase/comment/change_status.go
+++ go-comments/internal/usecase/comment/change_status.go
@@ -6,20 +6,20 @@ import (
 	dom "github.com/ee-crocush/go-news/go-comments/internal/domain/comment"
 )
 
-var _ ChangeStatusContract = (*ChangeUseCase)(nil)
+var _ ChangeStatusContract = (*ChangeStatusUseCase)(nil)
 
 // ChangeUseCase представляет структуру, реализующую бизнес-логику для изменения статуса комментария.
-type ChangeUseCase struct {
+type ChangeStatusUseCase struct {
 	repo dom.Repository
 }
 
 // NewChangeUseCase создает новый экземпляр usecase для изменения статуса комментария.
-func NewChangeUseCase(repo dom.Repository) *ChangeUseCase {
-	return &ChangeUseCase{repo: repo}
+func NewChangeStatusUseCase(repo dom.Repository) *ChangeStatusUseCase {
+	return &ChangeStatusUseCase{repo: repo}
 }
 
 // Execute выполняет бизнес-логику изменения статуса комментария.
-func (uc *ChangeUseCase) Execute(ctx context.Context, in StatusDTO) error {
+func (uc *ChangeStatusUseCase) Execute(ctx context.Context, in StatusDTO) error {
 	commentID, err := dom.NewID(in.ID)
 	if err != nil {
 		return fmt.Errorf("ChangeUseCase.NewID: %w", err)
